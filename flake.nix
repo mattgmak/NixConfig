810c8bcbec1a3bf47d0cdf309105e52b5dd58f04
mattgmak <u3592095@connect.hku.hk>
2025-01-31 23:02:00 +0800

    Setup working hyprland with home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    hyprland.url = "github:hyprwm/Hyprland";
+    xremap-flake.url = "github:xremap/nix-flake";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,14 +13,24 @@
     };
   };
 
-  outputs = { self, nixpkgs, zen-browser, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+  outputs = { self, nixpkgs, zen-browser, home-manager, ... }@inputs:
+    let
+      # lib = nixpkgs.lib;
+      # system = "x86_64-linux";
+      # pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.goofy = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.goofy = import ./home.nix;
+          }
+        ];
+      };
     };
-  };
 }
--- home.nix
+++ home.nix
@@ -1,76 +1,85 @@
 { config, pkgs, ... }:
 
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+let
+  startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
+    ${pkgs.waybar}/bin/waybar &
+  '';
+in {
   home.username = "goofy";
   home.homeDirectory = "/home/goofy";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [ hello ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  home.file = { };
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+  home.sessionVariables = { };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/root/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "ALT";
+    unbind = [];
+    bind = [
+      "$mod, Return, exec, wezterm"
+      "$mod, h, exec, rofi -show drun"
+      "$mod, P, exec, rofi -show pmenu"
+      "$mod, j, movefocus, u"
+      "$mod, k, movefocus, d"
+      "$mod, l, movefocus, l"
+      "$mod, semicolon, movefocus, r"
+      "$mod, u, resizeactive, 0 -10.0"
+      "$mod, i, resizeactive, 0 10.0"
+      "$mod, o, resizeactive, -10.0 0"
+      "$mod, p, resizeactive, 10.0 0"
+      "$mod, UP, movefocus, u"
+      "$mod, DOWN, movefocus, d"
+      "$mod, LEFT, movefocus, l"
+      "$mod, RIGHT, movefocus, r"
+      "$mod SHIFT, j, movewindow, u"
+      "$mod SHIFT, k, movewindow, d"
+      "$mod SHIFT, l, movewindow, l"
+      "$mod SHIFT, semicolon, movewindow, r"
+      "$mod SHIFT, UP, movewindow, u"
+      "$mod SHIFT, DOWN, movewindow, d"
+      "$mod SHIFT, LEFT, movewindow, l"
+      "$mod SHIFT, RIGHT, movewindow, r"
+      "$mod, Tab, cyclenext"
+      "$mod, t, togglefloating"
+      "$mod, f, fullscreen"
+      "$mod, d, killactive"
+      "$mod, q, exit"
+    ];
+    # bindm = [ "$mod, mouse:272, movewindow" "$mod, mouse:273, resizewindow" ];
+    monitor = ", highres, 0x0, 1";
+    input = {
+      kb_layout = "us";
+      follow_mouse = 0;
+      touchpad = {
+        natural_scroll = true;
+        disable_while_typing = 1;
+        scroll_factor = 0.5;
+      };
+      sensitivity = 0.5;
+    };
+    exec-once = "${startupScript}/bin/start";
+    xwayland = {
+      enabled = true;
+      force_zero_scaling = true;
+    };
+    decoration = {
+      rounding = 6;
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 3;
+      };
+    };
+    general = {
+      gaps_in = 2;
+      gaps_out = 2;
+    };
+    cursor = { no_warps = true; };
+  };
 }
