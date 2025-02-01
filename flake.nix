e5608e5a0bea99af8de7aeab53c952c4062c0797
mattgmak <u3592095@connect.hku.hk>
2025-02-01 17:08:24 +0800

    Move home-manager to home-manager/

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.goofy = import ./home.nix;
+            home-manager.users.goofy = import ./home-manager/home.nix;
           }
         ];
       };
---
+++ home-manager/home.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }: {
+  imports = [ ./hyprland.nix ];
+  home.username = "goofy";
+  home.homeDirectory = "/home/goofy";
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.packages = with pkgs; [ hello ];
+  programs.home-manager.enable = true;
+}
