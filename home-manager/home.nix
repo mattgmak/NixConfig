3dcc7058bfb58b591babf29ed2a249a029602293
mattgmak <u3592095@connect.hku.hk>
2025-02-05 21:28:33 +0800

    Add nushell imports

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
-  imports = [ ./hyprland.nix ];
+  imports = [ ./hyprland.nix ./nushell.nix ];
   home.username = "goofy";
   home.homeDirectory = "/home/goofy";
   home.stateVersion = "24.11"; # Please read the comment before changing.
