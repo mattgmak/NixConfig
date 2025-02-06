32d93df1ecac19467721086c29c049e471337da4
mattgmak <u3592095@connect.hku.hk>
2025-02-06 20:30:41 +0800

    Change nixpkgs to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
     hyprland.url = "github:hyprwm/Hyprland";
