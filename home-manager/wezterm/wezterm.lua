7f5ffd261ad7b759faf994fc1959be15be0557a8
mattgmak <u3592095@connect.hku.hk>
2025-02-27 13:49:01 +0800

    Add bar plugin for wezterm

--- home-manager/wezterm/wezterm.lua
+++ home-manager/wezterm/wezterm.lua
@@ -203,4 +203,62 @@ config.initial_cols = 80
 config.enable_wayland = false
 config.front_end = "WebGpu"
 
+-- bar plugin
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config, {
+    position = "bottom",
+    max_width = 32,
+    padding = {
+        left = 1,
+        right = 1
+    },
+    separator = {
+        space = 1,
+        left_icon = wez.nerdfonts.fa_long_arrow_right,
+        right_icon = wez.nerdfonts.fa_long_arrow_left,
+        field_icon = wez.nerdfonts.indent_line
+    },
+    modules = {
+        tabs = {
+            active_tab_fg = 4,
+            inactive_tab_fg = 6
+        },
+        workspace = {
+            enabled = true,
+            icon = wez.nerdfonts.cod_window,
+            color = 8
+        },
+        leader = {
+            enabled = true,
+            icon = wez.nerdfonts.oct_rocket,
+            color = 2
+        },
+        pane = {
+            enabled = true,
+            icon = wez.nerdfonts.cod_multiple_windows,
+            color = 7
+        },
+        username = {
+            enabled = true,
+            icon = wez.nerdfonts.fa_user,
+            color = 6
+        },
+        hostname = {
+            enabled = true,
+            icon = wez.nerdfonts.cod_server,
+            color = 8
+        },
+        clock = {
+            enabled = true,
+            icon = wez.nerdfonts.md_calendar_clock,
+            color = 5
+        },
+        cwd = {
+            enabled = true,
+            icon = wez.nerdfonts.oct_file_directory,
+            color = 7
+        }
+    }
+})
+
 return config
