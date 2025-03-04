d2a0475812140db78c468170f3c18d9ea7fb544e
mattgmak <u3592095@connect.hku.hk>
2025-03-04 19:44:15 +0800

    Move nushell config to nushell/config

--- home-manager/nushell/default.nix
+++ home-manager/nushell/default.nix
@@ -1,7 +1,8 @@
 { ... }: {
   home.file = {
-    ".config/nushell/config.nu".source = ./config.nu;
-    ".config/nushell/env.nu".source = ./env.nu;
-    ".config/nushell/scripts/conda.nu".source = ./scripts/conda.nu;
+    ".config/nushell" = {
+      source = ./config;
+      recursive = true;
+    };
   };
 }
