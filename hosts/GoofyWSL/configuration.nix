967d7658c972cc95b7e5ce19a5f6e7acc7a952b9
mattgmak <u3592095@connect.hku.hk>
2025-03-10 16:14:54 +0800

    Add nix-ld to WSL

--- hosts/GoofyWSL/configuration.nix
+++ hosts/GoofyWSL/configuration.nix
@@ -1,8 +1,12 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [ ../common.nix inputs.nixos-wsl.nixosModules.default ];
   wsl = {
     enable = true;
     defaultUser = "goofy";
+  };
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
   };
 
   system.stateVersion = "24.11";
