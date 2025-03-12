11fe42d842136587615d5cf6f217766de62284d7
mattgmak <172981@gmail.com>
2025-03-12 14:12:57 +0800

    Add nvidia drivers

--- hosts/GoofyVM/default.nix
+++ hosts/GoofyVM/default.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   imports = [ ../common.nix ./hardware-configuration.nix ];
   virtualisation.virtualbox.guest.enable = true;
-  system.copySystemConfiguration = true;
   services.openssh.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 2;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl = {
+    enable = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   environment.systemPackages = with pkgs; [
     firefox
