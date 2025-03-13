19cb7430cef9825097bf55f147888550181f87a8
mattgmak <u3592095@connect.hku.hk>
2025-03-13 17:13:54 +0800

    Update lazygit config

--- home-manager/lazygit/default.nix
+++ home-manager/lazygit/default.nix
@@ -1,15 +1,33 @@
-{
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      keybindings = {
-        prevItem-alt = "j";
-        nextItem-alt = "k";
-        prevBlock-alt = "l";
-        nextBlock-alt = "';'";
-        scrollUpMain-alt1 = "J";
-        scrollDownMain-alt1 = "K";
-      };
-    };
-  };
+{ config, ... }: {
+  programs.lazygit = { enable = true; };
+  stylix.targets.lazygit.enable = false;
+  home.file.".config/lazygit/config.yml".text = ''
+    gui:
+      theme:
+        activeBorderColor:
+        - '#${config.lib.stylix.colors.base0E}'
+        - bold
+        cherryPickedCommitBgColor:
+        - '#383a62'
+        cherryPickedCommitFgColor:
+        - '#666699'
+        defaultFgColor:
+        - '#f1eff8'
+        inactiveBorderColor:
+        - '#a0a0c5'
+        optionsTextColor:
+        - '#${config.lib.stylix.colors.base0B}'
+        searchingActiveBorderColor:
+        - '#${config.lib.stylix.colors.base0E}'
+        - bold
+        selectedLineBgColor:
+        - '#666699'
+        unstagedChangesColor:
+        - '#a0a0c5'
+    keybindings:
+      nextItem-alt: k
+      prevItem-alt: j
+      scrollDownMain-alt1: K
+      scrollUpMain-alt1: J
+  '';
 }
