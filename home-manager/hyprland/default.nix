be952df5bb880f497e61a37cb86806b516339b85
mattgmak <u3592095@connect.hku.hk>
2025-03-16 11:53:32 +0800

    Update Hyprland animations

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -174,10 +174,12 @@
       };
       animations = {
         enabled = "yes";
-        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        # bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        bezier = "myBezier, 0.10, 0.9, 0.1, 1.05";
         animation = [
           "windows, 1, 5, myBezier"
-          "windowsOut, 1, 7, default, popin 80%"
+          "windowsOut, 1, 5, myBezier"
+          # "windowsOut, 1, 7, default, popin 80%"
           "border, 1, 10, default"
           "fade, 1, 7, default"
           "workspaces, 1, 6, default"
