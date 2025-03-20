1caa5326afaedea7c4e42925a163c45d7bf5e19c
mattgmak <u3592095@connect.hku.hk>
2025-03-20 13:10:43 +0800

    Add zen defaults

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -36,6 +36,15 @@
     defaultApplications = {
       "application/pdf" = [ "okularApplication_pdf.desktop" ];
       "text/html" = [ "zen.desktop" ];
+      "x-scheme-handler/http" = [ "zen.desktop" ];
+      "x-scheme-handler/https" = [ "zen.desktop" ];
+      "x-scheme-handler/chrome" = [ "zen.desktop" ];
+      "application/x-extension-htm" = [ "zen.desktop" ];
+      "application/x-extension-html" = [ "zen.desktop" ];
+      "application/x-extension-shtml" = [ "zen.desktop" ];
+      "application/xhtml+xml" = [ "zen.desktop" ];
+      "application/x-extension-xhtml" = [ "zen.desktop" ];
+      "application/x-extension-xht" = [ "zen.desktop" ];
     };
   };
   programs.home-manager.enable = true;
