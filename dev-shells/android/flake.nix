9fca60c80ae0f5673cbea49143392f29cc2667c6
mattgmak <172981@gmail.com>
2025-03-23 08:55:51 +0800

    add android dev shell

---
+++ dev-shells/android/flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Android dev shell";
+  inputs = { nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; }; };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell { packages = with pkgs; [ android-tools ]; };
+    };
+}
