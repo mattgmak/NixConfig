f415ce1242081eed13f5f76355464439d895fab4
mattgmak <u3592095@connect.hku.hk>
2025-03-30 15:24:56 +0800

    Fix git credential helper for WSL

--- home-manager/terminal/git/default.nix
+++ home-manager/terminal/git/default.nix
@@ -5,12 +5,10 @@
     userEmail = "u3592095@connect.hku.hk";
     extraConfig = {
       fetch.prune = true;
-
-      # Set credential helper conditionally based on hostname
-      credential.helper = if hostname == "GoofyWSL" then
-        "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe"
-      else
-        null;
-    };
+    } // (if hostname == "GoofyWSL" then {
+      credential.helper =
+        "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+    } else
+      { });
   };
 }
