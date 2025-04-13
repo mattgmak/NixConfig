d5d02ace12d6c7d9652f4a846a2c83faca3b1c73
mattgmak <u3592095@connect.hku.hk>
2025-04-13 16:48:31 +0800

    Update battery notification script

--- home-manager/desktop/hyprland/hypr/scripts/battery-notification.sh
+++ home-manager/desktop/hyprland/hypr/scripts/battery-notification.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-notify-send --urgency=LOW "Battery Notification" "Starting..."
+notify-send --urgency=LOW -t 10000 "Battery Notification" "Started"
 while true; do
   bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
-  if [ "$bat_lvl" -le 15 ]; then
+  bat_status=$(cat /sys/class/power_supply/BAT1/status)
+  if [ "$bat_lvl" -le 15 ] && [ "$bat_status" = "Discharging" ]; then
     notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
     sleep 1200
-  elif [ "$bat_lvl" -gt 90 ]; then
+  elif [ "$bat_lvl" -lt 30 ] && [ "$bat_status" = "Discharging" ]; then
+    notify-send --urgency=NORMAL "Battery Warning" "Level: ${bat_lvl}%"
+    sleep 600
+  elif [ "$bat_lvl" -gt 90 ] && [ "$bat_status" = "Charging" ]; then
     notify-send --urgency=LOW "Battery Charged" "Level: ${bat_lvl}%"
     sleep 1200
   else
