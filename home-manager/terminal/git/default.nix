d23e453d67ef4b23745c28906c43099219830c84
mattgmak <u3592095@connect.hku.hk>
2025-04-22 17:02:57 +0800

    Enable rerere

--- home-manager/terminal/git/default.nix
+++ home-manager/terminal/git/default.nix
@@ -5,6 +5,7 @@
     userEmail = "u3592095@connect.hku.hk";
     extraConfig = {
       fetch.prune = true;
+      rerere.enabled = true;
     } // (if hostname == "GoofyWSL" then {
       credential.helper =
         "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
