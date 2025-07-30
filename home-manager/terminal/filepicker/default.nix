8f4e50f9e1f07d012788a976672b115a15a3a4af
mattgmak <u3592095@connect.hku.hk>
2025-07-30 22:50:12 +0800

    Update filepicker to use merged version

--- home-manager/terminal/filepicker/default.nix
+++ home-manager/terminal/filepicker/default.nix
@@ -6,16 +6,6 @@
   in {
     enable = true;
     package = termfilepickers;
-    config = {
-      terminal_command = lib.getExe pkgs.ghostty;
-      # terminal_command = "${lib.getExe pkgs.ghostty} -e";
-      # terminal_command = lib.getExe
-      #   (pkgs.writeShellScriptBin "ghostty-filepicker" ''
-      #     ${lib.getExe pkgs.ghostty} -e "$@"
-      #   '');
-      open_file_script_path = ./yazi-open-file.nu;
-      save_file_script_path = ./yazi-save-file.nu;
-      save_files_script_path = ./yazi-save-file.nu;
-    };
+    config = { terminal_command = [ (lib.getExe pkgs.ghostty) "-e" ]; };
   };
 }
