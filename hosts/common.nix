9b62c803ca4fc341730ea111f30853dbf7d3dbdc
mattgmak <u3592095@connect.hku.hk>
2025-08-28 22:24:23 +0800

    Fix hyprland package version

--- hosts/common.nix
+++ hosts/common.nix
@@ -224,6 +224,10 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    package =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   # Add flatpak support
