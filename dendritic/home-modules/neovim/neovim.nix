475627bce94e126544bd832f55c8320c9223c2fb
mattgmak <u3592095@connect.hku.hk>
2026-03-07 23:57:01 +0800

    Use stylix for nvim

--- dendritic/home-modules/neovim/neovim.nix
+++ dendritic/home-modules/neovim/neovim.nix
@@ -2,20 +2,27 @@
   flake.homeModules.neovim =
     { pkgs, ... }:
     {
-      home.file = {
-        ".config/nvim" = {
-          source = ./config;
-          recursive = true;
-        };
-      };
+      # home.file = {
+      #   ".config/nvim" = {
+      #     source = ./config;
+      #     recursive = true;
+      #   };
+      # };
       programs.neovim = {
         enable = true;
         withPython3 = true;
         withNodeJs = true;
+        extraLuaConfig = ''
+          ${builtins.readFile ./config/lua/plugins.lua}
+          ${builtins.readFile ./config/lua/config.lua}
+        '';
         extraPackages = with pkgs; [
           gcc
           tree-sitter
         ];
       };
+      home.packages = with pkgs; [
+        tree-sitter
+      ];
     };
 }
