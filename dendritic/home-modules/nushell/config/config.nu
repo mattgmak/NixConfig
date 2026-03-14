25cdb9e2c575f498ff4a37255f9201b252c9aabe
mattgmak <u3592095@connect.hku.hk>
2026-03-14 15:08:18 +0800

    Fix y alias ordering with zoxide integration

--- dendritic/home-modules/nushell/config/config.nu
+++ dendritic/home-modules/nushell/config/config.nu
@@ -925,16 +925,6 @@ $env.config = {
 }
 
 
-def --env y [...args] {
-	let tmp = (mktemp -t "yazi-cwd.XXXXXX")
-	yazi ...$args --cwd-file $tmp
-	let cwd = (open $tmp)
-	if $cwd != "" and $cwd != $env.PWD {
- 		z $cwd
-	}
-	rm -fp $tmp
-}
-
 def --env c [...args] {
     clear
 }
--- dendritic/home-modules/nushell/nushell.nix
+++ dendritic/home-modules/nushell/nushell.nix
@@ -1,15 +1,22 @@
 {
   flake.homeModules.nushell =
-    { username, pkgs, ... }:
+    {
+      username,
+      pkgs,
+      lib,
+      ...
+    }:
     {
       programs.nushell = {
         enable = true;
-        configFile.text = builtins.readFile ./config/config.nu;
-        envFile.text = builtins.readFile ./config/env.nu;
+        configFile.source = ./config/config.nu;
+        envFile.source = ./config/env.nu;
+        # To order the extra config after zoxide with default (1000)
+        extraConfig = lib.mkOrder 1100 (builtins.readFile ./config/extra.nu);
         environmentVariables = {
-          NH_OS_FLAKE = pkgs.lib.mkIf pkgs.stdenv.isLinux "/home/${username}/NixConfig";
-          NH_DARWIN_FLAKE = pkgs.lib.mkIf pkgs.stdenv.isDarwin "/Users/${username}/NixConfig#darwinConfigurations.MacMini";
-          DEVELOPER_DIR = pkgs.lib.mkIf pkgs.stdenv.isDarwin "/Applications/Xcode.app/Contents/Developer";
+          NH_OS_FLAKE = lib.mkIf pkgs.stdenv.isLinux "/home/${username}/NixConfig";
+          NH_DARWIN_FLAKE = lib.mkIf pkgs.stdenv.isDarwin "/Users/${username}/NixConfig#darwinConfigurations.MacMini";
+          DEVELOPER_DIR = lib.mkIf pkgs.stdenv.isDarwin "/Applications/Xcode.app/Contents/Developer";
         };
       };
     };
