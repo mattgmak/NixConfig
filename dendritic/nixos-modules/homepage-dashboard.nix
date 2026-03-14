838f4f377913a0cd6451d9b8f7a45007904874af
mattgmak <u3592095@connect.hku.hk>
2026-03-14 20:13:08 +0800

    Change allowed hosts

--- dendritic/nixos-modules/homepage-dashboard.nix
+++ dendritic/nixos-modules/homepage-dashboard.nix
@@ -1,11 +1,15 @@
 {
-  flake.nixosModules.homepage-dashboard = {
-    services.homepage-dashboard = {
-      enable = true;
-      openFirewall = false;
-      allowedHosts = "*";
+  flake.nixosModules.homepage-dashboard =
+    let
+      port = 8082;
+    in
+    {
+      services.homepage-dashboard = {
+        enable = true;
+        openFirewall = false;
+        allowedHosts = "100.111.11.128:${toString port},goofeus:${toString port}";
+      };
+      # Allow Tailscale nodes to access homepage-dashboard (port 8082)
+      networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ port ];
     };
-    # Allow Tailscale nodes to access homepage-dashboard (port 8082)
-    networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 8082 ];
-  };
 }
