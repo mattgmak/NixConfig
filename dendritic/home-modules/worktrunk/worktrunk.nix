89ddc310bfd53f25ef96da8a36ce4ff9d7e0f083
mattgmak <u3592095@connect.hku.hk>
2026-04-08 10:27:11 +0800

    Add path copying

--- dendritic/home-modules/worktrunk/worktrunk.nix
+++ dendritic/home-modules/worktrunk/worktrunk.nix
@@ -1,11 +1,22 @@
 { inputs, ... }:
 {
-  flake.homeModules.worktrunk = {
-    imports = [ inputs.worktrunk.homeModules.default ];
-    programs.worktrunk = {
-      enable = true;
-      enableNushellIntegration = true;
+  flake.homeModules.worktrunk =
+    { pkgs, ... }:
+    {
+      imports = [ inputs.worktrunk.homeModules.default ];
+      programs.worktrunk = {
+        enable = true;
+        enableNushellIntegration = true;
+      };
+      home.file.".config/worktrunk/config.toml".text = ''
+        # toml
+        [post-start]
+        copy = "wt step copy-ignored"
+
+        [post-switch]
+        copy-pwd = "printf %s {{ worktree_path }} | ${
+          if pkgs.stdenv.isDarwin then "/usr/bin/pbcopy" else "${pkgs.wl-clipboard}/bin/wl-copy"
+        }"
+      '';
     };
-    home.file.".config/worktrunk/config.toml".source = ./config.toml;
-  };
 }
