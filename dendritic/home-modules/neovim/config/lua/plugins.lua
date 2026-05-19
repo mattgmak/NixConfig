56693af7a9c1b24ae8513091b30904eb3df8e4c2
mattgmak <u3592095@connect.hku.hk>
2026-05-19 12:32:59 +0800

    Add pi-nvim

--- dendritic/home-modules/neovim/config/lua/plugins.lua
+++ dendritic/home-modules/neovim/config/lua/plugins.lua
@@ -219,6 +219,18 @@ require('lazy').setup({
     },
   },
   {
+    'carderne/pi-nvim',
+    cond = not is_vscode,
+    config = function()
+      require('pi-nvim').setup()
+      vim.keymap.set('n', '<leader>pp', ':PiSend<CR>', { desc = 'Send prompt to pi' })
+      vim.keymap.set('n', '<leader>pf', ':PiSendFile<CR>', { desc = 'Send file to pi' })
+      vim.keymap.set('v', '<leader>ps', ':PiSendSelection<CR>', { desc = 'Send selection to pi' })
+      vim.keymap.set('n', '<leader>pb', ':PiSendBuffer<CR>', { desc = 'Send buffer to pi' })
+      vim.keymap.set('n', '<leader>pi', ':PiPing<CR>', { desc = 'Ping pi session' })
+    end,
+  },
+  {
     'saghen/blink.cmp',
     -- dependencies = { 'rafamadriz/friendly-snippets' },
     cond = not is_vscode,
