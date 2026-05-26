214b3d465ad9a2a1cb5dfbff336ddba98afe1a09
mattgmak <u3592095@connect.hku.hk>
2026-05-26 14:33:37 +0800

    Use neovim as default

--- dendritic/home-modules/lazygit.nix
+++ dendritic/home-modules/lazygit.nix
@@ -24,16 +24,21 @@
           };
           os =
             let
-              cursorOpenCmd = "nu ~/.config/yazi/scripts/cursor-open.nu";
+              # cursorOpenCmd = "nu ~/.config/yazi/scripts/cursor-open.nu";
             in
             {
               open = if pkgs.stdenv.isDarwin then "open {{filename}}" else "xdg-open {{filename}}";
               openLink = if pkgs.stdenv.isDarwin then "open {{link}}" else "xdg-open {{link}}";
-              edit = "${cursorOpenCmd} {{filename}}";
-              editAtLine = "${cursorOpenCmd} {{filename}}:{{line}}";
-              editAtLineAndWait = "${cursorOpenCmd} {{filename}}:{{line}}";
-              editInTerminal = false;
-              openDirInEditor = "${cursorOpenCmd} {{dir}}";
+              edit = "nvim {{filename}}";
+              editAtLine = "nvim {{filename}} -c 'call cursor({{line}}, 0)'";
+              editAtLineAndWait = "nvim {{filename}} -c 'call cursor({{line}}, 0)'";
+              editInTerminal = true;
+              openDirInEditor = "nvim {{dir}}";
+              # edit = "${cursorOpenCmd} {{filename}}";
+              # editAtLine = "${cursorOpenCmd} {{filename}}:{{line}}";
+              # editAtLineAndWait = "${cursorOpenCmd} {{filename}}:{{line}}";
+              # editInTerminal = false;
+              # openDirInEditor = "${cursorOpenCmd} {{dir}}";
             };
         };
       };
