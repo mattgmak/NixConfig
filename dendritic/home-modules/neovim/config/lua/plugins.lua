b239e08166b7f9f154e72ada51d1351bb831b368
mattgmak <u3592095@connect.hku.hk>
2026-05-31 15:33:17 +0800

    Add cursorline

--- dendritic/home-modules/neovim/config/lua/plugins.lua
+++ dendritic/home-modules/neovim/config/lua/plugins.lua
@@ -751,4 +751,24 @@ require('lazy').setup({
     },
     opts_extend = { 'sources.default' },
   },
+  {
+    'ya2s/nvim-cursorline',
+    cond = not is_vscode,
+    config = function()
+      require('nvim-cursorline').setup({
+        disable_filetypes = {},
+        disable_buftypes = {},
+        cursorline = {
+          enable = true,
+          timeout = 500,
+          number = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 3,
+          hl = { underline = true },
+        },
+      })
+    end,
+  },
 })
