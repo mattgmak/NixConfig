149df82dab88fea1e0c794f1c4b6c259248e52dc
mattgmak <u3592095@connect.hku.hk>
2026-06-05 17:56:55 +0800

    Add plugins

--- dendritic/home-modules/neovim/config/lua/plugins.lua
+++ dendritic/home-modules/neovim/config/lua/plugins.lua
@@ -803,4 +803,67 @@ require('lazy').setup({
       })
     end,
   },
+  {
+    'rachartier/tiny-code-action.nvim',
+    event = 'LspAttach',
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    opts = {
+      backend = 'delta',
+      picker = 'telescope',
+      backend_opts = {
+        delta = {
+          header_lines_to_remove = 4,
+          args = {
+            '--line-numbers',
+          },
+        },
+      },
+      resolve_timeout = 100,
+      notify = {
+        enabled = true,
+        on_empty = true,
+      },
+      signs = {
+        quickfix = { '', { link = 'DiagnosticWarning' } },
+        others = { '', { link = 'DiagnosticWarning' } },
+        refactor = { '', { link = 'DiagnosticInfo' } },
+        ['refactor.move'] = { '󰪹', { link = 'DiagnosticInfo' } },
+        ['refactor.extract'] = { '', { link = 'DiagnosticError' } },
+        ['source.organizeImports'] = { '', { link = 'DiagnosticWarning' } },
+        ['source.fixAll'] = { '󰃢', { link = 'DiagnosticError' } },
+        ['source'] = { '', { link = 'DiagnosticError' } },
+        ['rename'] = { '󰑕', { link = 'DiagnosticWarning' } },
+        ['codeAction'] = { '', { link = 'DiagnosticWarning' } },
+      },
+    },
+    config = function()
+      vim.keymap.set(
+        { 'n', 'x' },
+        '<leader>ca',
+        function() require('tiny-code-action').code_action({}) end,
+        { noremap = true, silent = true }
+      )
+    end,
+  },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy',
+    priority = 1000,
+    config = function()
+      require('tiny-inline-diagnostic').setup({
+        options = {
+          multilines = {
+            enabled = true,
+          },
+          show_source = {
+            enabled = true,
+          },
+          override_open_float = true,
+        },
+      })
+      vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+    end,
+  },
 })
