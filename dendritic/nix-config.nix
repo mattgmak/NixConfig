0cd4e4d7bd58e3fc6e64079daf2524aa218849aa
mattgmak <u3592095@connect.hku.hk>
2026-06-09 21:20:52 +0800

    Fix nix cache

--- dendritic/nix-config.nix
+++ dendritic/nix-config.nix
@@ -9,14 +9,16 @@
             "https://nix-community.cachix.org"
             "https://nix-gaming.cachix.org"
             "https://yazi.cachix.org"
-            "https://cache.nixos-cuda.org"
+            # "https://cache.nixos-cuda.org"
+            "https://cuda-maintainers.cachix.org"
           ];
           trusted-public-keys = [
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
             "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+            # "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           ];
           warn-dirty = false;
           experimental-features = [
