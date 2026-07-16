7197811a830c59ca57f3c4e7c17505e5c07f27b5
mattgmak <u3592095@connect.hku.hk>
2026-07-16 11:34:40 +0800

    Use oil as default

--- dendritic/home-modules/neovim/config/lua/plugins/oil.lua
+++ dendritic/home-modules/neovim/config/lua/plugins/oil.lua
@@ -3,9 +3,12 @@ local is_vscode = vim.g.vscode ~= nil
 return {
   'stevearc/oil.nvim',
   cond = not is_vscode,
-  event = 'VeryLazy',
+  lazy = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup({})
+    require('oil').setup({
+      default_file_explorer = true,
+    })
     vim.keymap.set('n', '<leader>ff', '<cmd>Oil<cr>', { desc = 'Oil file browser' })
   end,
 }
