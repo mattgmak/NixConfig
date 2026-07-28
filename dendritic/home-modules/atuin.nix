1f3fe3ead1aedb5119ec98e725499679f45467c7
mattgmak <u3592095@connect.hku.hk>
2026-07-28 10:04:29 +0800

    Add ctrl n shortcuts to atuin

--- dendritic/home-modules/atuin.nix
+++ dendritic/home-modules/atuin.nix
@@ -1,47 +1,39 @@
 {
   flake.homeModules.atuin =
-    { hostname, username, ... }:
+    {
+      lib,
+      hostname,
+      username,
+      ...
+    }:
+    let
+      acceptShiftShortcuts = lib.listToAttrs (
+        map (n: {
+          name = "ctrl-shift-${toString n}";
+          value = "accept-${toString n}";
+        }) (lib.range 1 9)
+      );
+    in
     {
       programs.atuin = {
         enable = true;
         daemon.enable = hostname != "Droid" && username != "root" && hostname != "MacMini";
         enableBashIntegration = false;
         settings = {
+          ctrl_n_shortcuts = true;
           exit_mode = "return-query";
           invert = true;
           enter_accept = true;
           keymap = {
-            emacs."ctrl-d" = "delete";
-            vim-normal."ctrl-d" = "delete";
-            emacs."ctrl-y" = "copy";
-            vim-normal."ctrl-y" = "copy";
+            emacs = acceptShiftShortcuts // {
+              "ctrl-d" = "delete";
+              "ctrl-y" = "copy";
+            };
+            vim-normal = acceptShiftShortcuts // {
+              "ctrl-d" = "delete";
+              "ctrl-y" = "copy";
+            };
           };
-          # keymap.prefix =
-          #   let
-          #     commandSlots = lib.map (index: toString index) (lib.range 1 9);
-          #     acceptList = lib.map (index: {
-          #       name = "${index}";
-          #       value = "accept-${index}";
-          #     }) commandSlots;
-          #     returnSelectionList = lib.map (index: {
-          #       name = "ctrl-${index}";
-          #       value = "return-selection-${index}";
-          #     }) commandSlots;
-          #   in
-          #   builtins.listToAttrs (acceptList ++ returnSelectionList);
-          # keymap.emacs =
-          #   let
-          #     commandSlots = lib.map (index: toString index) (lib.range 1 9);
-          #     acceptList = lib.map (index: {
-          #       name = "ctrl-${index}";
-          #       value = "accept-${index}";
-          #     }) commandSlots;
-          #     returnSelectionList = lib.map (index: {
-          #       name = "ctrl-shift-${index}";
-          #       value = "return-selection-${index}";
-          #     }) commandSlots;
-          #   in
-          #   builtins.listToAttrs (acceptList ++ returnSelectionList);
         };
       };
     };
