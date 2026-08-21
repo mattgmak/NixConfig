3c74158ce85c5fe58efe93da25c8c1e8d8f5b601
mattgmak <u3592095@connect.hku.hk>
2026-08-21 23:58:41 +0800

    Add nix-community substituter to Droid nix config.

--- dendritic/hosts/Droid.nix
+++ dendritic/hosts/Droid.nix
@@ -112,9 +112,11 @@
       nix = {
         substituters = [
           "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
           "https://yazi.cachix.org"
         ];
         trustedPublicKeys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
         ] ++ self.builderPublicKeys;
       };
