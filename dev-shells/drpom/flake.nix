594eec7d8af96a596f8c90ed536b978c762038aa
mattgmak <u3592095@connect.hku.hk>
2026-08-24 16:41:09 +0800

    Hide biome pin

--- dev-shells/drpom/flake.nix
+++ dev-shells/drpom/flake.nix
@@ -203,7 +203,7 @@
                     [ ]
                 );
               NODE_OPTIONS = "--experimental-vm-modules";
-              BIOME_BINARY = "${biome-pin-pkgs.biome}/bin/biome";
+              # BIOME_BINARY = "${biome-pin-pkgs.biome}/bin/biome";
               shellHook = ''
                 export NODE_COMPILE_CACHE=~/.cache/nodejs-compile-cache
                 ${
